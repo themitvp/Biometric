b7367a8655e3e0e99eb69df721a0504278730602
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2016-06-16 14:56:08 +0200

    Added code

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_legacy -lopencv_gpu -lopencv_flann -lopencv_features2d -lopencv_contrib -lopencv_calib3d -lopencv_core -lopencv_imgproc -lopencv_videostab -lopencv_video -lopencv_ts -lopencv_superres -lopencv_stitching -lopencv_photo -lopencv_ocl -lopencv_objdetect -lopencv_nonfree -lopencv_ml -lopencv_highgui
+
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/facerec_demo.cpp 
+
+OBJS += \
+./src/facerec_demo.o 
+
+CPP_DEPS += \
+./src/facerec_demo.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/local/Cellar/opencv/2.4.13/include -O0 -g3 -Wall -c -fmessage-length=0 -v -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
